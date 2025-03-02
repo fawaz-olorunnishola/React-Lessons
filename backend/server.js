c51732e4a8a97a9eaa94df66bd4a674aca953d60
Fawas Olorunnishola <fawasolorunnishola9@gmail.com>
2025-03-02 23:54:01 +0100

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+import { connectDB } from "./config/db.js"
 
 
 // app config
@@ -10,11 +11,17 @@ const port = 4000
 app.use(express.json())
 app.use(cors())
 
+
+// db connection
+connectDB();
+
 app.get("/",(req,res)=>{
     res.send("API Working")
 }) 
 
 app.listen(port,()=>{
-    console.log(`Server started on http://localhost:${port}`);
+    console.log(`Server started on http://localhost:${port}`)
     
 })
+
+// mongodb+srv://Greatstack:<db_password>@cluster0.w4jwl.mongodb.net/?
