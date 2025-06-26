f69a7197e504236631dc412c17734253fefb7ca4
Fawas Olorunnishola <Fawas Olorunnishola>
2025-06-26 15:00:13 +0100

    Updated project folders

--- Frontend/src/assets/assets.js
+++ Frontend/src/assets/assets.js
@@ -8,6 +8,9 @@ import menu_x2 from './menu_x2.png'
 import menu_dell from './menu_dell.png'
 import menu_pb from './menu_pb.png'
 import menu_len from './menu_len.png'
+import menu_desk from './menu_desk.png'
+import menu_logi from './menu_logi.png'
+import menu_game from'./menu_game.png'
 import menu_5 from './menu_5.png'
 import menu_6 from './menu_6.png'
 import menu_7 from './menu_7.png'
@@ -34,6 +37,22 @@ import len_ideap_flex from './len_ideap_flex.png'
 import len_thinkb_14 from './len_thinkb_14.png'
 import len_thinkp_E16 from './len_thinkp_E16.png'
 import len_legion from './len_legion.png'
+import hp_laser_pro_4003dn from './hp_laser_pro_4003dn.png'
+import hp_laser_pro_m15w from './hp_laser_pro_m15w.png'
+import hp_laserjet_pro_m226dn from './hp_laserjet_pro_m226dn.png'
+import hp_officejet_pro_8135e from './hp_officejet_pro_8135e.png'
+import logi_m170 from './logi_m170.png'
+import logi_m185 from './logi_m185.png'
+import logi_g305_lightspeed from './logi_g305_lightspeed.png'
+import logi_pro_lightspeed from './logi_pro_lightspeed.png'
+import game_acer_nitro5 from './game_acer_nitro5.png'
+import game_alienware_m15 from './game_alienware_m15.png'
+import game_alienware_x15 from './game_alienware_x15.png'
+import game_asus_a15 from './game_asus_a15.png'
+import game_dell_G16 from './game_dell_G16.png'
+import game_hp_omen16 from './game_hp_omen16.png'
+import game_legion_7i from './game_legion_7i.png'
+import game_omen_16 from './game_omen_16.png'
 import hp_840 from './hp_840.png'
 import food_5 from './food_5.png'
 import food_6 from './food_6.png'
@@ -125,16 +144,16 @@ export const menu_list = [
         menu_image: menu_len
     },
     {
-        menu_name: "printers",
-        menu_image: menu_6
+        menu_name: "Printers",
+        menu_image: menu_desk
     },
     {
-        menu_name: "logitech",
-        menu_image: menu_7
+        menu_name: "Logitech",
+        menu_image: menu_logi
     },
     {
         menu_name: "Gaming laptops",
-        menu_image: menu_8
+        menu_image: menu_game
     },
 ]
 
@@ -143,7 +162,7 @@ export const food_list = [
         _id: "1",
         name: "Hp Pavillion 15",
         image: hp_pav_15,
-        price: 12,
+        price: 0,
         description: "Hp Essentials",
         category: "Pavillion"
     },
@@ -151,77 +170,77 @@ export const food_list = [
         _id: "2",
         name: "Hp pavillion x360",
         image: hp_x360,
-        price: 18,
+        price: 0,
         description: "Hp Essentials",
         category: "Pavillion"
     }, {
         _id: "3",
         name: "Hp Pavillion i7",
         image: hp_i7,
-        price: 16,
+        price: 0,
         description: "Hp Essentials",
         category: "Pavillion"
     }, {
         _id: "4",
         name: "Hp pavillion 14",
         image: hp_pav_14,
-        price: 24,
+        price: 0,
         description: "Hp Essentials",
         category: "Pavillion"
     }, {
         _id: "5",
         name: "Hp spectre 13 x360",
         image: spectre_13_x360,
-        price: 14,
+        price: 0,
         description: "Hp Essentials",
         category: "spectre"
     }, {
         _id: "6",
         name: "Hp spectre 14",
         image: spectre_14,
-        price: 12,
+        price: 0,
         description: "Hp Essentials",
         category: "spectre"
     }, {
         _id: "7",
         name: "Hp spectre x360 Flip",
         image: spectre_x360_evo,
-        price: 20,
+        price: 0,
         description: "Hp Essentials",
         category: "spectre"
     }, {
         _id: "8",
         name: "Hp spectre x360",
         image: spectre_x360,
-        price: 15,
+        price: 0,
         description: "Hp Essentials",
         category: "spectre"
     }, {
         _id: "9",
         name: "Dell 15 3000",
         image: dell_15_3000,
-        price: 14,
+        price: 0,
         description: "Dell Essentials",
         category: "Dell"
     }, {
         _id: "10",
         name: "Dell 15 3521",
         image: dell_15_3521,
-        price: 22,
+        price: 0,
         description: "Dell Essentials",
         category: "Dell"
     }, {
         _id: "11",
         name: "Dell Latitude",
         image: dell_i_3000,
-        price: 10,
+        price: 0,
         description: "Dell Essentials",
         category: "Dell"
     }, {
         _id: "12",
         name: "Dell xps 13",
         image: dell_xps_13,
-        price: 12,
+        price: 0,
         description: "Dell Essentials",
         category: "Dell"
     },
@@ -229,7 +248,7 @@ export const food_list = [
         _id: "13",
         name: "Hp probook 440 G9",
         image: hp_pb_450_G9,
-        price: 12,
+        price: 0,
         description: "Hp Essentials",
         category: "Hp-probooks"
     },
@@ -237,144 +256,168 @@ export const food_list = [
         _id: "14",
         name: "Hp  probook 450 G15",
         image: hp_pb_450_g615,
-        price: 18,
+        price: 0,
         description: "Hp Essentials",
         category: "Hp-probooks"
     }, {
         _id: "15",
         name: "Hp probook 440 G10",
         image: hp_pb_440_g10,
-        price: 16,
+        price: 0,
         description: "Hp Essentials",
         category: "Hp-probooks"
     }, {
         _id: "16",
         name: "Hp probook 840 G7",
         image: hp_840_g10,
-        price: 24,
+        price: 0,
         description: "Hp Essentials",
         category: "Hp-probooks"
     }, {
         _id: "17",
         name: "Lenovo ideapad flex",
         image: len_ideap_flex,
-        price: 14,
+        price: 0,
         description: "Lenovo Essentials",
         category: "Lenovo"
     }, {
         _id: "18",
         name: "Lenovo ThinkBook 14",
         image: len_thinkb_14,
-        price: 12,
+        price: 0,
         description: "Lenovo Essentials",
         category: "Lenovo"
     }, {
         _id: "19",
         name: "Lenovo ThinkPad E16",
         image: len_thinkp_E16,
-        price: 20,
+        price: 0,
         description: "Lenovo Essentials",
         category: "Lenovo"
     }, {
         _id: "20",
         name: "Lenovo Legion",
         image: len_legion,
-        price: 15,
+        price: 0,
         description: "Lenovo Essentials",
         category: "Lenovo"
     }, {
         _id: "21",
-        name: "Garlic Mushroom ",
-        image: hp_840,
-        price: 14,
+        name: "Hp Laserjet Pro 4003dn",
+        image: hp_laser_pro_4003dn,
+        price: 0,
         description: "Hp Essentials",
-        category: "Pure Veg"
+        category: "Printers"
     }, {
         _id: "22",
-        name: "Fried Cauliflower",
-        image: hp_840,
-        price: 22,
+        name: "Hp Laserjet Pro m15dw",
+        image: hp_laser_pro_m15w,
+        price: 0,
         description: "Hp Essentials",
-        category: "Pure Veg"
+        category: "Printers"
     }, {
         _id: "23",
-        name: "Mix Veg Pulao",
-        image: hp_840,
-        price: 10,
+        name: "Hp Laserjet Pro M226DN",
+        image: hp_laserjet_pro_m226dn,
+        price: 0,
         description: "Hp Essentials",
-        category: "Pure Veg"
+        category: "Printers"
     }, {
         _id: "24",
-        name: "Rice Zucchini",
-        image: hp_840,
-        price: 12,
+        name: "Hp Officejet pro 8135e",
+        image: hp_officejet_pro_8135e,
+        price: 0,
         description: "Hp Essentials",
-        category: "Pure Veg"
+        category: "Printers"
     },
     {
         _id: "25",
-        name: "Hp Spectre",
-        image: hp_840,
-        price: 12,
-        description: "Hp Essentials",
-        category: "Laptops"
+        name: "Logitech M170",
+        image: logi_m170,
+        price: 0,
+        description: "Logitech Essentials",
+        category: "Logitech"
     },
     {
         _id: "26",
-        name: "Hp Spectre",
-        image: hp_840,
-        price: 18,
-        description: "Hp Essentials",
-        category: "Laptops"
+        name: "Logitech M185",
+        image: logi_m185,
+        price: 0,
+        description: "Logitech Essentials",
+        category: "Logitech"
     }, {
         _id: "27",
-        name: "Hp Spectre",
-        image: hp_840,
-        price: 16,
-        description: "Hp Essentials",
-        category: "Laptops"
+        name: "Logitech G305 Lightspeed",
+        image: logi_g305_lightspeed,
+        price: 0,
+        description: "Logitech Essentials",
+        category: "Logitech"
     }, {
         _id: "28",
-        name: "Hp Spectre",
-        image: hp_840,
-        price: 24,
-        description: "Hp Essentials",
-        category: "Laptops"
+        name: "Logi Pro Lightspeed",
+        image: logi_pro_lightspeed,
+        price: 0,
+        description: "Logitech Essentials",
+        category: "Logitech"
     }, {
         _id: "29",
-        name: "Hp Elitebook",
-        image: hp_840,
-        price: 14,
-        description: "Hp Essentials",
-        category: "Laptops"
+        name: "Acer Nitro",
+        image: game_acer_nitro5,
+        price: 0,
+        description: "Gaming Essentials",
+        category: "Gaming laptops"
     }, {
         _id: "30",
-        name: "Hp Elitebook",
-        image: hp_840,
-        price: 12,
-        description: "Hp Essentials",
-        category: "Laptops"
+        name: "Alienware M15",
+        image: game_alienware_m15,
+        price: 0,
+        description: "Gaming Essentials",
+        category: "Gaming laptops"
     }, {
         _id: "31",
-        name: "Hp Elitebook",
-        image: hp_840,
-        price: 20,
-        description: "Hp Essentials",
-        category: "Laptops"
+        name: "Alienware X15",
+        image: game_alienware_x15,
+        price: 0,
+        description: "Gaming Essentials",
+        category: "Gaming laptops"
     }, {
         _id: "32",
-        name: "Hp Elitebook",
-        image: hp_840,
-        price: 15,
-        description: "Hp Essentials",
-        category: "Laptops"
+        name: "Asus A15",
+        image: game_asus_a15,
+        price: 0,
+        description: "Gaming Essentials",
+        category: "Gaming laptops"
     }
     , {
         _id: "33",
-        name: "Hp",
-        image: hp_840,
-        price: 15,
-        description: "HP Essentials",
-        category: "Laptops"
+        name: "Dell G16",
+        image: game_dell_G16,
+        price: 0,
+        description: "Gaming Essentials",
+        category: "Gaming laptops"
+    }
+    , {
+        _id: "33",
+        name: "Hp Omen 16",
+        image: game_hp_omen16,
+        price: 0,
+        description: "Gaming Essentials",
+        category: "Gaming laptops"
+    }
+    , {
+        _id: "33",
+        name: "Legion 7i",
+        image: game_legion_7i,
+        price: 0,
+        description: "Gaming Essentials",
+        category: "Gaming laptops"
+    }
+    , {
+        _id: "33",
+        name: "Game Omen 16",
+        image: game_omen_16,
+        price: 0,
+        description: "Gaming Essentials",
+        category: "Gaming laptops"
     }
 ]
--- Frontend/src/pages/Cart/Cart.jsx
+++ Frontend/src/pages/Cart/Cart.jsx
@@ -28,9 +28,9 @@ const Cart = () => {
                 <div className="cart-items-title cart-items-item">
                   <img src={item.image} alt="" />
                   <p>{item.name}</p>
-                  <p>${item.price}</p>
+                  <p>&#8358;{item.price}</p>
                   <p>{cartItems[item._id]}</p>
-                  <p>${item.price * cartItems[item._id]}</p>
+                  <p>&#8358;{item.price * cartItems[item._id]}</p>
                   <p onClick={() => removeFromCart(item._id)} className="cross">
                     x
                   </p>
@@ -47,20 +47,24 @@ const Cart = () => {
           <div>
             <div className="cart-total-details">
               <p>Subtotal</p>
-              <p>${getTotalCartAmount()}</p>
+              <p>&#8358;{getTotalCartAmount()}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>${getTotalCartAmount()===0?0:2}</p>
+              <p>&#8358;{getTotalCartAmount() === 0 ? 0 : 2}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+              <b>
+                &#8358;{getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}
+              </b>
             </div>
           </div>
-          <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
+          <button onClick={() => navigate("/order")}>
+            PROCEED TO CHECKOUT
+          </button>
         </div>
         <div className="cart-promocode">
           <div>
